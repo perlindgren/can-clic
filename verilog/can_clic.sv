8727962ef310128afe5af8832b2c55defa4c3200
Per Lindgren <per.lindgren@ltu.se>
2024-02-14 19:27:05 +0100

    hmm

--- verilog/can_clic.sv
+++ verilog/can_clic.sv
@@ -7,13 +7,82 @@ module can_clic #(
     output logic [INDEX_BITS-1:0] index
 );
   logic [PRIO_BITS-1:0] p;
+  logic [INDEX_BITS-1:0][0:0] contender;
+  logic or_value;
+
   always begin
     for (integer i = 0; i < 2 ** (INDEX_BITS - 1); i++) begin
-      p = entries[i];
+      p = entries[i];  // later enabled and pending
       $display("i %3d prio %3b", i, p);
-
+      contender[i] = 1'b1;  // initially set true
     end
-    {is_interrupt, index} = {1'b0, 2'b00};
+
+    // sanity check
+    for (integer i = 0; i < 2 ** (INDEX_BITS - 1); i++) begin
+      $display("i %3d contender %0b", i, contender[i]);
+    end
+
+
+    for (integer j = PRIO_BITS - 1; j >= 0; j--) begin
+      or_value = 1'b0;
+      for (integer i = 0; i < 2 ** (INDEX_BITS - 1); i++) begin
+
+        p = entries[i];
+
+        $display("j %3d, i %3d, p = %3b p[j] = %0b", j, i, p, p[j]);
+
+        if (contender[i] & p[j]) begin
+
+          $display("or_value %0b, - write 1 -", or_value);
+          or_value = 1'b1;
+
+        end
+      end
+
+      for (integer i = 0; i < 2 ** (INDEX_BITS - 1); i++) begin
+        p = entries[i];
+        if (or_value & !p[j]) begin
+          // it is ok to loose multiple times
+          $display("i %d looses", i);
+          contender[i] = 0;
+        end
+      end
+    end
+
+    // for (integer j = INDEX_BITS - 1; j >= 0; j--) begin
+    //   or_value = 1'b0;
+    //   for (integer i = 0; i < 2 ** (INDEX_BITS - 1); i++) begin
+    //     $display("j %3d, i %3d, ii = %3b p[j] = %0b", j, i, p, p[j]);
+
+    //     if (contender[i] & p[j]) begin
+
+    //       $display("or_value %0b, - write 1 -", or_value);
+    //       or_value = 1'b1;
+
+    //     end
+    //   end
+
+    //   for (integer i = 0; i < 2 ** (INDEX_BITS - 1); i++) begin
+    //     p = entries[i];
+    //     if (or_value & !p[j]) begin
+    //       // it is ok to loose multiple times
+    //       $display("i %d looses", i);
+    //       contender[i] = 0;
+    //     end
+    //   end
+    // end
+
+    is_interrupt = 0;
+    index = '{default: '0};
+    for (integer i = 0; i < 2 ** (INDEX_BITS - 1); i++) begin
+      if (contender[i]) begin
+        assert (!is_interrupt)
+        else $error("multiple winners");
+        is_interrupt = 1'b1;
+        index = i[1:0];
+        $display("winner is i %d", i);
+      end
+    end
   end
 endmodule
 
--- verilog/tb_can_clic.sv
+++ verilog/tb_can_clic.sv
@@ -15,7 +15,12 @@ module tb_can_clic;
     $dumpfile("waveform.vcd");
     $dumpvars;
 
-    entries = {{3'b010}, {3'b100}};
+    //entries = {{3'b000}, {3'b001}, {3'b000}, {3'b001}};
+    entries = {{2'b000}, {2'b001}};
+    // #10 entries = {{3'b000}, {3'b000}};
+    // #10 entries = {{3'b001}, {3'b000}};
+    // #10 entries = {{3'b000}, {3'b001}};
+    // #10 entries = {{3'b001}, {3'b001but}};
     #10 $finish;
 
   end
