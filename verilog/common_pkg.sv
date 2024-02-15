c00ce1e758abf8869367db8f31b8df14bf2848e8
Per Lindgren <per.lindgren@ltu.se>
2024-02-16 00:39:40 +0100

    2*3 bits

--- verilog/common_pkg.sv
+++ verilog/common_pkg.sv
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
 package common_pkg;
-  parameter NR_PRIO_BITS = 3;
-  parameter NR_INDEX_BITS = 2;
+  parameter NR_PRIO_BITS = 2;
+  parameter NR_INDEX_BITS = 3;
   // parameter MAX_INDEX = (2 ** NR_INDEX_BITS);
 
   typedef logic [NR_PRIO_BITS-1:0] Entries[2**NR_INDEX_BITS-1:0];
--- verilog/tb_can_clic.sv
+++ verilog/tb_can_clic.sv
@@ -19,30 +19,30 @@ module tb_can_clic;
     $dumpvars;
 
     // highest entry (leftmost), represent the threshold
-    entries = {{3'b001}, {3'b000}, {3'b000}, {3'b001}};
+    entries = {{2'b01}, {2'b00}, {2'b00}, {2'b01}, {2'b01}, {2'b00}, {2'b00}, {2'b01}};
     #10 $display("(0), is_interrupt %d, index %d", is_interrupt, index);
-    assert (is_interrupt == 0 && index == 3) $display("filtered by threshold");
+    assert (is_interrupt == 0 && index == 7) $display("filtered by threshold");
     else $error("should be filtered by threshold");
 
-    entries = {{3'b000}, {3'b000}, {3'b000}, {3'b000}};
+    entries = {{2'b00}, {2'b00}, {2'b00}, {2'b01}, {2'b01}, {2'b00}, {2'b00}, {2'b01}};
     #10 $display("(0), is_interrupt %d, index %d", is_interrupt, index);
-    assert (is_interrupt == 0 && index == 3) $display("filtered by threshold");
+    assert (is_interrupt == 1 && index == 4) $display("ok");
     else $error("should be filtered by threshold");
 
-    entries = {{3'b000}, {3'b011}, {3'b010}, {3'b001}};
-    #10 $display("(1), is_interrupt %d, index %d", is_interrupt, index);
-    assert (is_interrupt == 1 && index == 2) $display("ok");
-    else $error("should interrupt");
+    // entries = {{3'b000}, {3'b011}, {3'b010}, {3'b001}};
+    // #10 $display("(1), is_interrupt %d, index %d", is_interrupt, index);
+    // assert (is_interrupt == 1 && index == 2) $display("ok");
+    // else $error("should interrupt");
 
-    entries = {{3'b011}, {3'b101}, {3'b010}, {3'b110}};
-    #10 $display("(1), is_interrupt %d, index %d", is_interrupt, index);
-    assert (is_interrupt == 1 && index == 0) $display("ok");
-    else $error("should interrupt");
+    // entries = {{3'b011}, {3'b101}, {3'b010}, {3'b110}};
+    // #10 $display("(1), is_interrupt %d, index %d", is_interrupt, index);
+    // assert (is_interrupt == 1 && index == 0) $display("ok");
+    // else $error("should interrupt");
 
-    entries = {{3'b011}, {3'b101}, {3'b110}, {3'b100}};
-    #10 $display("(1), is_interrupt %d, index %d", is_interrupt, index);
-    assert (is_interrupt == 1 && index == 1) $display("ok");
-    else $error("should interrupt");
+    // entries = {{3'b011}, {3'b101}, {3'b110}, {3'b100}};
+    // #10 $display("(1), is_interrupt %d, index %d", is_interrupt, index);
+    // assert (is_interrupt == 1 && index == 1) $display("ok");
+    // else $error("should interrupt");
 
   end
 
